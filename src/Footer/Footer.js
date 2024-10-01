809e309b6b5f3c77a18a8e93f5d51efdd3ac3968
Praveenkumar512 <143414253+Praveenkumar512@users.noreply.github.com>
2024-10-01 12:32:16 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer class="bottom">
+        <div class="center">
+            All rights reserved &copy; Praveen Kumar Meka
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
